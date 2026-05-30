d6de3ec5852acb63851ebd8ae11c882b9256aa33
Chaitanya Krishna <chaitanyachaithu370@gmail.com>
2026-05-30 10:28:50 +0530

    Switch to PostgreSQL

--- facemorphattacks/settings.py
+++ facemorphattacks/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'facemorphattacks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
